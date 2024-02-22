949c4e1dbfff2863d15bef2499cead6da4a88fb9
962027 <962027@PITLAB2178776.senac.educ>
2024-02-22 10:44:14 -0300

    atualizaçao

---
+++ vendas/src/model/bean/funcionario.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Samuelson
+ */
+public class funcionario {
+    
+   
+    private String nome;
+    private String email;
+    private String celular;
+    private String telefone;
+    private String cep;
+    private String end;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String complemento;
+    private String uf;
+    private String rg;
+    private String cpf;
+    private String cargo;
+    private String senha;
+    private String nivel;
+    
+    public String getnome(){
+        return nome;
+    }
+     public void setnome(String nome){
+        this.nome = nome;
+    }
+     public String getemail(){
+        return email;
+    }
+    
+    public void setemail(String email){
+        this.email = email;
+    }
+    
+    public String getcelular(){
+        return celular;
+    }
+    
+    public void setcelular(String celular){
+        this.celular = celular;
+    }
+    
+    public String gettelefone(){
+        return telefone;
+    }
+    
+    public void settelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public String getcep(){
+        return cep;
+    }
+    
+    public void setcep(String cep){
+        this.cep = cep;
+    }
+    
+    public String getend(){
+        return end;
+    }
+    
+    public void setend(String end){
+        this.end = end;
+    }
+    
+    public String getnumero(){
+        return numero;
+    }
+    
+    public void setnumero(String numero){
+        this.numero = numero;
+    }
+    
+     public String getbairro(){
+        return bairro;
+    }
+    
+    public void setbairro(String bairro){
+        this.bairro = bairro;
+    }
+    
+     public String getcidade(){
+        return cidade;
+    }
+    
+    public void setcidade(String cidade){
+        this.cidade = cidade;
+    }
+    
+     public String getcomplemento(){
+        return complemento;
+    }
+    
+    public void setcomplemento(String complemento){
+        this.complemento = complemento;
+    }
+    
+     public String getuf(){
+        return uf;
+    }
+    
+    public void setuf(String uf){
+        this.uf = uf;
+    }
+    
+     public String getrg(){
+        return rg;
+    }
+    
+    public void setrg(String rg){
+        this.rg = rg;
+    }
+    
+     public String getcpf(){
+        return cpf;
+    }
+    
+    public void setcpf(String cpf){
+        this.cpf = cpf;
+    }
+    
+     public String getcargo(){
+        return cargo;
+    }
+    
+    public void setcargo(String cargo){
+        this.cargo = cargo;
+    }
+    
+     public String getsenha(){
+        return senha;
+    }
+    
+    public void setsenha(String senha){
+        this.senha = senha;
+    }
+    
+     public String getnivel(){
+        return nivel;
+    } 
+    
+    public void setnivel(String nivel){
+        this.nivel = nivel;
+    }
+    
+    
+            
+    
+    
+    
+
+   
+    
+    
+}
